cf6cea21d827defe6a7e8e81e1ab567624716e6d
andymunoz <admunoz8@miners.utep.edu>
2020-02-19 14:44:35 -0700

    Add Edit

---
+++ java1/src/SnakeGame.java
@@ -0,0 +1,33 @@
+public class SnakeGame {
+    private boolean[][] game;
+    private int[] headPosition;
+    private static int exhaustiveChecks;
+    private static int recursiveChecks;
+
+    public SnakeGame(){
+        game = new boolean [0][0];
+
+    }
+
+    public int[] findTailExhaustive(){
+
+
+
+    }
+    public int[] findTailRecursive(){
+
+    }
+    private int[] findTailRecursive(int[] currentPosition, int[] previousPosition){
+
+    }
+    private void resetCounters(){
+
+    }
+    private static int getRecursiveChecks(){
+
+    }
+    private static int getExhaustiveChecks(){
+
+    }
+
+}
