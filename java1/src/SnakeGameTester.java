9667f4f015ddd72617454a7a02c6ce38ccdc48a4
Admunoz8 <60449029+Admunoz8@users.noreply.github.com>
2020-02-28 19:05:29 -0700

    Add files via upload

--- java1/src/SnakeGameTester.java
+++ java1/src/SnakeGameTester.java
@@ -1,110 +1,137 @@
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-class SnakeGameTester {
-    boolean[][] board1 = {{false, false, false},
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SnakeGameTester {
+
+    boolean[][] board1 = {{false, true, false},
             {false, true, false},
             {false, true, false}};
-    SnakeGame game1 = new SnakeGame(board1, 1, 2);
+    SnakeGame game1 = new SnakeGame(board1, 1,1);
 
-    boolean[][] board2 = {{false, false, false},
-            {false, false, false},
-            {false, false, false}};
-    SnakeGame game2 = new SnakeGame(board2, 2, 2);
-
-    boolean[][] board3 = {{true, true, true},
-            {true, true, true},
+    boolean[][] board2 = {{true, true, true},
+            {false, false, true},
             {true, true, true}};
-    SnakeGame game3 = new SnakeGame(board3, 1, 1);
+    SnakeGame game2 = new SnakeGame(board2, 0,0);
 
-    boolean[][] board4 = {{true, false, true},
-            {true,false, true},
-            {true, false, true}};
-    SnakeGame game4 = new SnakeGame(board4, 1, 1);
+    boolean[][] board3 = {{false, false, false, false, false},
+            {true, true, true, true, true},
+            {true, true, true, true, true},
+            {false, false, false, false, false}};
+    SnakeGame game3 = new SnakeGame(board3, 3,3);
 
-    boolean[][] board5 = {{false, true, false},
-            {false, true, false},
-            {false, true, false}};
-    SnakeGame game5 = new SnakeGame(board5, 1, 1);
+    boolean[][] board4 = {{false, false}};
 
+    SnakeGame game4 = new SnakeGame(board4, 1,0);
+
+    boolean[][] board5 = {{false, false, false, false, false, false},
+            {false, false, false, false, false, false},
+            {false, false, false, false, false, false},
+            {false, false, true, false, false, false},
+            {false, false, false, false, false, false},
+            {false, false, false, false, false, false},
+            {false, false, false, false, false, false},
+            {false, false, false, false, false, false},
+            {false, false, false, false, false, false},
+            {false, false, false, false, false, false}};
+    SnakeGame game5 = new SnakeGame(board5, 3,2);
 
 
 
     @Test
-    public void testFindTailExhaustive() {
-        int[] expected = new int[]{2, 1, 2};
+    public void testFindTailExhaustive1(){
+        int[] expected = new int[]{0, 1, 3};
         int[] actual = game1.findTailExhaustive();
 
+        assertEquals(2, game1.getExhaustiveChecks());
         assertArrayEquals(expected, actual);
     }
+
+
     @Test
-    public void testFindTailExhaustive1() {
-        int[] expected = new int[]{2, 2, 1};
+    public void testFindTailExhaustive2(){
+        int[] expected = new int[]{2, 0, 7};
         int[] actual = game2.findTailExhaustive();
 
+        assertEquals(7, game2.getExhaustiveChecks());
         assertArrayEquals(expected, actual);
     }
+
+
     @Test
-    public void testFindTailExhaustive2() {
-        int[] expected = new int[]{0, 2, 3};
+    public void testFindTailExhaustive3(){
+        int[] expected = new int[]{0, 0, 0};
         int[] actual = game3.findTailExhaustive();
 
         assertArrayEquals(expected, actual);
     }
+
+    //im trying to see if it works with any board and has  no true values
     @Test
-    public void testFindTailExhaustive3() {
-        int[] expected = new int[]{0, 0, 3};
+    public void testFindTailExhaustive4(){
+        int[] expected = new int[]{0,0,0};
         int[] actual = game4.findTailExhaustive();
 
         assertArrayEquals(expected, actual);
     }
+
+
+
+     // Test case 5: it tests what happens when the head is equal to the tail
+
     @Test
-    public void testFindTailExhaustive4() {
-        int[] expected = new int[]{0, 2, 3};
+    public void testFindTailExhaustive5(){
+        int[] expected = new int[]{0,0,1};
         int[] actual = game5.findTailExhaustive();
 
         assertArrayEquals(expected, actual);
     }
+
+
     @Test
-    public void testFindrecursive() {
-        int[] expected = new int[]{1, 1, 2};
+    public void testFindTailRecursive1(){
+        int[] expected = new int[]{1, 1 ,1};
         int[] actual = game1.findTailRecursive();
 
+        assertEquals(1, game1.getRecursiveChecks());
         assertArrayEquals(expected, actual);
-
     }
+
+
     @Test
-    public void testFindrecursive1() {
-        int[] expected = new int[]{2, 2, 1};
+    public void testFindTailRecursive2(){
+        int[] expected = new int[]{2, 0 ,2};
         int[] actual = game2.findTailRecursive();
 
+        assertEquals(6, game2.getRecursiveChecks());
         assertArrayEquals(expected, actual);
-
     }
+
+
     @Test
-    public void testFindrecursive2() {
-        int[] expected = new int[]{0, 0, 3};
+    public void testFindTailRecursive3(){
+        int[] expected = new int[]{3, 0 ,9};
         int[] actual = game3.findTailRecursive();
 
         assertArrayEquals(expected, actual);
-
     }
+
     @Test
-    public void testFindrecursive3() {
-        int[] expected = new int[]{0, 0, 3};
-        int[] actual = game4.findTailRecursive();
+    public void testFindTailRecursive4(){
+        int[] expected = new int[]{0,0,0};
+        int[] actual = game4.findTailExhaustive();
 
         assertArrayEquals(expected, actual);
-
     }
+
+
     @Test
-    public void testFindrecursive4() {
-        int[] expected = new int[]{0, 1, 2};
-        int[] actual = game5.findTailRecursive();
+    public void testFindTailRecursive5(){
+        int[] expected = new int[]{0,0,1};
+        int[] actual = game5.findTailExhaustive();
 
         assertArrayEquals(expected, actual);
-
     }
-
-
 }
-
+ //done
